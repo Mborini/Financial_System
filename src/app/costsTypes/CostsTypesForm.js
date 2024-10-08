c4cc7f9e4a6da520347407db75c9fcb684392b99
Mborini <mborini98@gmail.com>
2024-10-08 20:25:41 +0300

    fix

--- src/app/costsTypes/CostsTypesForm.js
+++ src/app/costsTypes/CostsTypesForm.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 
-export default function costsTypesForm({ refetchcostsTypes, setOpen }) {
+export default function CostsTypesForm({ refetchcostsTypes, setOpen }) {
   const [description, setDescription] = useState("");
   const [name, setName] = useState("");
   const [loading, setLoading] = useState(true);
@@ -19,9 +19,7 @@ export default function costsTypesForm({ refetchcostsTypes, setOpen }) {
 
     if (response.ok) {
       setDescription("");
-
       setName("");
-
       setOpen(false); // Close the drawer
       refetchcostsTypes(); // Refetch the table data
     }
@@ -66,8 +64,6 @@ export default function costsTypesForm({ refetchcostsTypes, setOpen }) {
           className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         />
       </div>
-
-      {/* Type field */}
 
       {/* Submit button */}
       <div>
