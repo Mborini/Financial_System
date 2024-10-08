5ea01bd13cb91d8fb476fa0125ee5ce76bb8adf0
Mborini <mborini98@gmail.com>
2024-10-08 21:28:42 +0300

    fix connection

--- src/app/purchases/EditPurchasesForm.js
+++ src/app/purchases/EditPurchasesForm.js
@@ -191,7 +191,7 @@ export default function EditPurchasesForm({
           value={supplier}
           onChange={(e) => setSupplier(e.target.value)}
           required
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          className="mt-1 block w-full px-3  py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         >
           <option value="">Select supplier</option>
           {loading ? (
